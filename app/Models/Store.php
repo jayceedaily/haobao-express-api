b6d5935344b3e854525c2ae804bc4f5940581e12
Jaycee Daily <alohajaycee@gmail.com>
2022-12-05 11:33:01 +0800

    Implement Store

---
+++ app/Models/Store.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    public function scopeOwned($query)
+    {
+        return $query->ownedBy(auth()->user());
+    }
+
+    public function scopeOwnedBy($query, User $user)
+    {
+        return $query->where('created_by', $user->id);
+    }
+
+    public function itemCategories()
+    {
+        return $this->hasMany(ItemCategory::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+}
